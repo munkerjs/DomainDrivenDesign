75ebc30b3c835c147f7a7e34a8057cf412f3b6e3
Münker Erkadam <munkererkadam@gmail.com>
2024-02-29 14:24:13 +0300

    Bug Fixed

--- DomainDrivenDesign.Infrastructure/Context/ApplicationDbContext.cs
+++ DomainDrivenDesign.Infrastructure/Context/ApplicationDbContext.cs
@@ -12,7 +12,7 @@ namespace DomainDrivenDesign.Infrastructure.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=DESKTOP-KEM5IF3;Database=DomainDrivenDesign;User Id=sa;Password=040418;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Connection Timeout=30;");
+            optionsBuilder.UseSqlServer("Server=DESKTOP-KEM5IF3;Database=DomainDrivenDesign;User Id=sa;Password=123456;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Connection Timeout=30;");
         }
 
         public DbSet<User> Users { get; set; }
